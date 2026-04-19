7ddbff3e853ac68699fcf5173ae0f2a7aefd2b24
Behnamrhp74 <behnam.rahimpoor@beatstream.com>
2026-04-20 00:49:37 +0300

    fix(pipeline): Bump package json version with semantic versioning

--- release.config.cjs
+++ release.config.cjs
@@ -63,20 +63,21 @@ const defaultPlugins = [
 function getReleasePlugins() {
   return [
     ...defaultPlugins,
+    ["@semantic-release/npm", { npmPublish: false }],
     [
       "@semantic-release/changelog",
       {
         changelogFile: "CHANGELOG.md",
         changelogTitle: "# Changelog",
+        preset: "conventionalcommits",
       },
     ],
-    ["@semantic-release/npm", { npmPublish: false }],
     [
       "@semantic-release/git",
       {
         assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
         message:
-          "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}",
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
     [
